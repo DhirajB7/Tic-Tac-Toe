b7d75a6c27d268d4a783f7a358c05315b9941355
Dhiraj <dhiraj.basavaraju@gmail.com>
2024-02-24 03:54:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-console.log("MY DEAR DAUGHTER, THIS IS FOR YOU ❤️")
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
